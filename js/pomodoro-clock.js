5debd75b788ebd452b7921133f98f064d8d1dbc6
Carolina Knoll <carolina-knoll@hotmail.com>
2016-01-19 20:04:44 -0200

    Indent code and fix breakTime value

--- js/pomodoro-clock.js
+++ js/pomodoro-clock.js
@@ -2,101 +2,91 @@
 /*global alert: false, console: false */
 
 var minutes = 25,
-  seconds = 60,
-  workTime = 25,
-  breakTime = 15,
-  isBreak = false,
-  timerOn = false;
+    seconds = 60,
+    workTime = 25,
+    breakTime = 5,
+    isBreak = false,
+    timerOn = false;
 
 function timerLoop() {
-   var wav = 'http://www.oringz.com/oringz-uploads/sounds-917-communication-channel.mp3';
-        var audio = new Audio(wav);
-			  
-  if (timerOn) {
-    $('.minutes').text(minutes);
-    if ((seconds) > 0) {
+    var wav = 'http://www.oringz.com/oringz-uploads/sounds-917-communication-channel.mp3';
+    var audio = new Audio(wav);
 
-      seconds -= 1;
-      if (seconds < 10) {
-        $('.seconds').text('0' + seconds);
-      } 
-      
-      else {
-        $('.seconds').text(seconds);
-      }
-
-      setTimeout(timerLoop, 1000);
-    } 
-    
-    else if ((minutes + seconds) > 0) {
-      minutes -= 1;
-      $('.minutes').text(minutes);
-      seconds = 60;
-      timerLoop();
-    } 
-    
-    else {
-      if (isBreak) {
-       audio.play();
-        minutes = workTime;
+    if (timerOn) {
         $('.minutes').text(minutes);
-        seconds = 60;
-        timerLoop();
-      } 
-      
-      else {
-        audio.play();
-        timeForBreak();
-        timerLoop();
-      }
+        if ((seconds) > 0) {
 
-    }
+            seconds -= 1;
+            if (seconds < 10) {
+                $('.seconds').text('0' + seconds);
+            } else {
+                $('.seconds').text(seconds);
+            }
 
-    function timeForBreak() {
-      isBreak = true;
-      minutes = breakTime;
-      $('.minutes').text(minutes);
-      seconds = 60;
+            setTimeout(timerLoop, 1000);
+        } else if ((minutes + seconds) > 0) {
+            minutes -= 1;
+            $('.minutes').text(minutes);
+            seconds = 60;
+            timerLoop();
+        } else {
+            if (isBreak) {
+                audio.play();
+                minutes = workTime;
+                $('.minutes').text(minutes);
+                seconds = 60;
+                timerLoop();
+            } else {
+                audio.play();
+                timeForBreak();
+                timerLoop();
+            }
+
+        }
+
+        function timeForBreak() {
+            isBreak = true;
+            minutes = breakTime;
+            $('.minutes').text(minutes);
+            seconds = 60;
+        }
     }
-  }
 }
 
-$('.breakPlus').click(function() {
-  breakTime += 1;
-  $('.breakTime').text(breakTime + ' min');
-});
-
-$('.breakMinus').click(function() {
-  if (breakTime > 0) {
-    breakTime -= 1;
+$('.breakPlus').click(function () {
+    breakTime += 1;
     $('.breakTime').text(breakTime + ' min');
-  }
 });
 
-$('.workPlus').click(function() {
-  workTime += 1;
-  minutes = workTime;
-  seconds = 60;
-  $('.workTime').text(workTime + ' min');
+$('.breakMinus').click(function () {
+    if (breakTime > 0) {
+        breakTime -= 1;
+        $('.breakTime').text(breakTime + ' min');
+    }
 });
 
-
-$('.workMinus').click(function() {
-  if (workTime > 0) {
-    workTime -= 1;
+$('.workPlus').click(function () {
+    workTime += 1;
     minutes = workTime;
     seconds = 60;
     $('.workTime').text(workTime + ' min');
-  }
 });
 
-$('.circleClock').click(function() {
-  if (timerOn) {
-    timerOn = false;
-  } 
-  
-  else {
-    timerOn = true;
-    timerLoop();
-  }
+
+$('.workMinus').click(function () {
+    if (workTime > 0) {
+        workTime -= 1;
+        minutes = workTime;
+        seconds = 60;
+        $('.workTime').text(workTime + ' min');
+    }
+});
+
+$('.circleClock').click(function () {
+    if (timerOn) {
+        timerOn = false;
+    } else {
+        timerOn = true;
+        timerLoop();
+    }
 });
