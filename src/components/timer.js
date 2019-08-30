a86e8fe79e69952a70fb36f1915ba46252f01e38
Carolina Knoll <carolinasknoll@gmail.com>
2019-08-30 10:08:29 -0300

    Add timer status message to indicate running/paused state. This will be different from the timerLabel message, which will only show on session/on break status.

--- src/components/timer.js
+++ src/components/timer.js
@@ -2,12 +2,18 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
 export default class Timer extends Component {
+  handleTimerStatusMessage() {
+    return this.props.timerRunning
+      ? 'Timer is running.'
+      : 'Timer is paused.'
+  }
 
   render() {
     return (
       <div id="start_stop" className="timer" onClick={this.props.controlTimerPause}>
         <h3 id="time-left" className="timer-length">{this.props.timeLeft}</h3>
         <h4 id="timer-label" className="status">{this.props.timerLabel}</h4>
+        <h4 id="timer-label" className="status">{this.handleTimerStatusMessage()}</h4>
       </div>
     )
   }
@@ -16,5 +22,6 @@ export default class Timer extends Component {
 Timer.propTypes = {
   controlTimerPause: PropTypes.func.isRequired,
   timeLeft: PropTypes.string.isRequired,
-  timerLabel: PropTypes.string.isRequired
+  timerLabel: PropTypes.string.isRequired,
+  timerRunning: PropTypes.bool.isRequired
 }
