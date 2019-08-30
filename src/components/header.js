77b6c5a70d4651717316838bfbffc51be70849ef
Carolina Knoll <carolinasknoll@gmail.com>
2019-08-30 08:23:11 -0300

    Remove Component import from function components (oops :P)

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,16 @@
-import React, {Component} from 'react';
+import React from 'react';
 
 export default function Header() {
   return (
     <div className="header-container">
       <h1 className="title">P<span>o</span>m<span>o</span>d<span>o</span>r<span>o</span> Timer</h1>
-      <h2>Online timer based on the <a href="https://en.wikipedia.org/wiki/Pomodoro_Technique" target="_blank"> Pomodoro Technique.</a></h2>
+
+      <h2>Online timer based on the
+        <a href="https://en.wikipedia.org/wiki/Pomodoro_Technique" target="_blank" rel="noopener noreferrer">
+          Pomodoro Technique.
+        </a>
+      </h2>
+
       <h2>Increase or decrease the session and break lengths below as needed, and click the timer to start.</h2>
       <h2>The timer will let you know when either a session or a break has ended by playing a sound.</h2>
     </div>
